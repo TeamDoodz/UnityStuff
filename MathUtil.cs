73a6e0c648f99b64033a099c2f287de3db4d1e3a
TeamDoodz <47047372+TeamDoodz@users.noreply.github.com>
2022-05-08 09:29:08 -0500

    Update MathUtil.cs

--- MathUtil.cs
+++ MathUtil.cs
@@ -1,5 +1,13 @@
 public static class MathUtil {
   public static float Remap(this float input, float inputMin, float inputMax, float min, float max) {
-		return min + (input - inputMin) * (max - min) / (inputMax - inputMin);
-	}
+    return min + (input - inputMin) * (max - min) / (inputMax - inputMin);
+  }
+	
+  public static bool EqualsRoughly(this Vector3 A, Vector3 other, float epsilon) {
+    return Vector3.Distance(A, other) < epsilon;
+  }
+
+  public static Vector3Int Round(this Vector3 A) {
+    return new Vector3Int(Mathf.RoundToInt(A.x), Mathf.RoundToInt(A.y), Mathf.RoundToInt(A.z));
+  } 
 }
